0313ee8fbdf902996c1f25c3e260d5f51fbb4329
Siva Golla <gvsivannarayana@gmail.com>
2024-06-24 22:43:54 +0100

    Cats TableView Cell view model tests

--- CatsDemo/CatsDemoTests/ACatViewModelTests.swift
+++ CatsDemo/CatsDemoTests/ACatViewModelTests.swift
@@ -11,30 +11,82 @@ import XCTest
 final class ACatViewModelTests: XCTestCase {
 
     var session: MockURLSession!
-    var viewModel: ACatViewModel!
+    var viewModel: CatsViewModel!
+    var group: DispatchGroup!
+    
     override func setUpWithError() throws {
         super.setUp()
+        
         session = MockURLSession()
-//        viewModel = ACatViewModel(model: <#T##CatBreed#>)
+        viewModel = CatsViewModel()
+        
+        group = DispatchGroup()
+        viewModel.group = group
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testViewModelParams() throws {
+        session.delegate = self
+        UserSession.activeSession = session
+        
+        let catsExpectation = expectation(description: "fetch all cats successful")
+        
+        viewModel.group.notify(queue: .main) {
+            catsExpectation.fulfill()
         }
+        
+        group.enter()
+        viewModel.loadCatList()
+        waitForExpectations(timeout: 5, handler: nil)
+        XCTAssert(viewModel.catModels.count > 0)
+        
+        let aViewModel = viewModel.catModels[2]
+        XCTAssertFalse(aViewModel.name.isEmpty)
+        XCTAssertFalse(aViewModel.isFavorite)
+        XCTAssertTrue(aViewModel.favID == 0)
     }
+    
+    func testViewModelFavCat() throws {
+        session.delegate = self
+        UserSession.activeSession = session
+        
+        let catsExpectation = expectation(description: "fetch all cats and favs successful")
+        
+        viewModel.itemsDidChange = {
+            catsExpectation.fulfill()
+        }
+        
+        viewModel.fetchAllCatsWithFavs()
+        waitForExpectations(timeout: 5, handler: nil)
+        XCTAssert(viewModel.catModels.count > 0)
+        XCTAssert(viewModel.favCats.count > 0)
+        
+        let aViewModel = viewModel.catModels[1]
+        XCTAssertFalse(aViewModel.name.isEmpty)
+        XCTAssertTrue(aViewModel.isFavorite)
+        XCTAssertTrue(aViewModel.favID > 0)
+    }
+}
 
+extension ACatViewModelTests: MockURLSessionDelegate {
+    func resourceName(for path: String, httpMethod: String) -> String {
+        if path.contains("breeds") {
+            return "FetchAllCatsResponse"
+        }
+        
+        if path.contains("favourites") {
+            if httpMethod == RequestMethod.get.rawValue {
+                return "MyFavCats"
+            }
+            
+            if httpMethod == RequestMethod.post.rawValue || httpMethod == RequestMethod.del.rawValue {
+                return "FavResponse"
+            }
+        }
+        
+        return ""
+    }
 }
